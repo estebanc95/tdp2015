f4660790a63679b418ae1bba2c5c1a84e23ad9e5
Esteban Federico <estebancanela@hotmail.com>
2015-10-21 17:15:52 -0300

    Modificacion moviemiento

--- Bomberman/src/entidades/AltairGrafica.java
+++ Bomberman/src/entidades/AltairGrafica.java
@@ -17,19 +17,19 @@ public class AltairGrafica extends EntidadGrafica {
 	
 	public void mover(int dir) {
 		switch (dir) {
-			case 1 : // Derecha
+			case 0 : // Derecha
 				this.pos.setLocation(pos.x + weight, pos.y);
 				super.mover(dir);
 				break;
-			case 2 : // Izquierda
+			case 1 : // Izquierda
 				this.pos.setLocation(pos.x - weight, pos.y);
 				super.mover(dir);
 				break;
-			case 3 : // Arriba
+			case 2 : // Arriba
 				this.pos.setLocation(pos.x, pos.y - height);
 				super.mover(dir);
 				break;
-			case 4 : // Abajo
+			case 3 : // Abajo
 				this.pos.setLocation(pos.x, pos.y + height);
 				super.mover(dir);
 				break;

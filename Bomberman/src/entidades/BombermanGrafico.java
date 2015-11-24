893647ad0e2893427b845c98f3fd155f4b7fbba8
Esteban Federico <estebancanela@hotmail.com>
2015-11-24 20:45:00 -0300

    Gif en Bomberman

--- Bomberman/src/entidades/BombermanGrafico.java
+++ Bomberman/src/entidades/BombermanGrafico.java
@@ -20,11 +20,11 @@ public class BombermanGrafico extends EntidadGrafica {
 	public BombermanGrafico(int vel, int x, int y) {
 		super(vel, x, y);
 		
-		this.mov[Direccion.DERECHA] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanDer.png"));
-		this.mov[Direccion.IZQUIERDA] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanIzq.png"));
-		this.mov[Direccion.ABAJO] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanAb.png"));
-		this.mov[Direccion.ARRIBA] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanAr.gif"));	
-		this.dmovn = new ImageIcon(this.getClass().getResource("/imagenes/BombermanAb.png"));
+		this.mov[Direccion.DERECHA] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanDer.gif"));
+		this.mov[Direccion.IZQUIERDA] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanIzq.gif"));
+		this.mov[Direccion.ABAJO] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanAr.gif"));
+		this.mov[Direccion.ARRIBA] = new ImageIcon(this.getClass().getResource("/imagenes/BombermanAb.gif"));	
+		this.dmovn = new ImageIcon(this.getClass().getResource("/imagenes/BombermanAr.gif"));
 	}
 	
 	
